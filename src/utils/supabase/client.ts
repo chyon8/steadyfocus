645674b9df6e8d25f73090d6c7a242a9f1dafa4b
chyon8 <chyon8@hotmail.com>
2025-12-27 20:43:16 +0900

    feat:env

--- src/utils/supabase/client.ts
+++ src/utils/supabase/client.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
-import { projectId, publicAnonKey } from './info';
-
-const supabaseUrl = `https://${projectId}.supabase.co`;
+const supabaseUrl = `https://${import.meta.env.VITE_SUPABASE_PROJECT_ID}.supabase.co`;
+const publicAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, publicAnonKey);
 
